debaef1bb5aa306c3ebb02957c44837d682891a0
Jan Janßen <janssen@mpie.de>
2021-03-19 16:35:21 +0100

    initial commit

---
+++ .ci_support/build_notebooks.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# pyiron config 
+python .ci_support/pyironconfig.py
+
+# install
+bash binder/postBuild
+
+
+# execute notebooks
+current_dir=$(pwd)
+i=0;
+for f in $(find . -name *.ipynb | sort -n); do
+    cd $(dirname $f);
+    notebook=$(basename $f);
+    papermill ${notebook} ${notebook%.*}-out.${notebook##*.} -k "python3" || i=$((i+1));
+    cd $current_dir;
+done;
+
+# push error to next level
+if [ $i -gt 0 ]; then
+    exit 1;
+fi;
