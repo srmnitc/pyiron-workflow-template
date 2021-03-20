073db436c9214a39778710a01e7e9f2f9c5e9827
Jan Janßen <janssen@mpie.de>
2021-03-20 07:22:34 +0100

    move pyiron resources to subfolder

--- .github/ci_support/import_dataset.sh
+++ .github/ci_support/import_dataset.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-for ds in $(ls calculation/*.tar.gz); do 
+for ds in $(ls pyiron/calculation/*.tar.gz); do 
     cp ${ds} .
     cp calculation/export.csv .
     file=$(basename ${ds} .tar.gz)
     python << EOF
 from pyiron_base import Project
-Project("calculation").unpack("${file}")
+Project("pyiron/calculation").unpack("${file}")
 EOF
     rm $(basename ${ds})
     rm export.csv
